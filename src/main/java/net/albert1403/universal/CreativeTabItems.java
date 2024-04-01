bf59b95d6127e81354744b664589f335ea0fe76b
A1403 <74734363+A1403@users.noreply.github.com>
2024-04-01 09:22:03 +0200

    -Removed the blocks

--- src/main/java/net/albert1403/universal/CreativeTabItems.java
+++ src/main/java/net/albert1403/universal/CreativeTabItems.java
@@ -15,29 +15,10 @@ public class CreativeTabItems {
 
     public static void buildContents(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(DungeonBlocks.stone_path);
-            event.accept(DungeonBlocks.encarved_stone);
-            event.accept(DungeonBlocks.dirty_stonefloor);
-            event.accept(DungeonBlocks.dirty_cobblestone);
-            event.accept(DungeonBlocks.chiseled_stone);
-            event.accept(DungeonBlocks.empty_bookshelf);
-            event.accept(DungeonBlocks.encarved_stone_bricks);
-            event.accept(DungeonBlocks.mossy_carved_stone_bricks);
-            event.accept(DungeonBlocks.mossy_cracked_stone_bricks);
-            event.accept(DungeonBlocks.mossy_smooth_andesite);
-            event.accept(DungeonBlocks.old_bookshelf);
-            event.accept(DungeonBlocks.rotten_planks);
-            event.accept(DungeonBlocks.stone_pillar);
-            event.accept(DungeonBlocks.charred_dirt);
-        }
+            }
         if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
-            event.accept(DungeonBlocks.spider_egg);
-            event.accept(DungeonBlocks.mossy_stone);
-            event.accept(DungeonBlocks.mossy_dirt);
-            event.accept(DungeonBlocks.egg_growth);
-            event.accept(DungeonBlocks.mossy_coarse_dirt);
-            event.accept(DungeonBlocks.mossy_andesite);
-
+            /*event.accept(DungeonBlocks.mossy_andesite);
+            */
         }
     }
 }
